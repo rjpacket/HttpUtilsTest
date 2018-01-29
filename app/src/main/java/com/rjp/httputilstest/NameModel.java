ec05f7342b2235e2cfc553a1b5aa504dd0efbbf0
Rjpacket <jimbo922@163.com>
2018-01-29 22:38:38 +0800

    update

---
+++ app/src/main/java/com/rjp/httputilstest/NameModel.java
@@ -0,0 +1,17 @@
+package com.rjp.httputilstest;
+
+/**
+ * Created by Administrator on 2018/1/29.
+ */
+
+public class NameModel {
+    private String femalename;
+
+    public String getFemalename() {
+        return femalename;
+    }
+
+    public void setFemalename(String femalename) {
+        this.femalename = femalename;
+    }
+}
